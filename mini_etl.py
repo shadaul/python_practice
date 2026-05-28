0a63337750d8093f31ea6257e7cc2b0b258f7932
daulet <shabakovdaulet@gmail.com>
2026-05-28 07:38:41 +0200

    working with data using python only

--- mini_etl.py
+++ mini_etl.py
@@ -30,17 +30,72 @@
 #         counts[layer] += 1
 # print(counts)
 
-raw_users = [
-    {"id": 1, "name": "Daulet", "age": 25},
-    {"id": 2, "name": "Karim", "age": 16},    # Младше 18
-    {"id": 3, "name": "Roman", "age": None},  # Возраст не указан
-    {"id": 4, "name": "Anna", "age": 30}
-]
+# raw_users = [
+#     {"id": 1, "name": "Daulet", "age": 25},
+#     {"id": 2, "name": "Karim", "age": 16},    # Младше 18
+#     {"id": 3, "name": "Roman", "age": None},  # Возраст не указан
+#     {"id": 4, "name": "Anna", "age": 30}
+# ]
 
-clean = []
+# clean = []
 
-for user in raw_users:
-    if user["age"] != None and user["age"] >= 18:
-        clean.append(user)
+# for user in raw_users:
+#     if user["age"] != None and user["age"] >= 18:
+#         clean.append(user)
 
-print(clean)
+# print(clean)
+
+# transactions = [
+#     {"id": 1, "amount_usd": 50.0, "status": "success"},
+#     {"id": 2, "amount_usd": 15.0, "status": "failed"},
+#     {"id": 3, "amount_usd": 100.0, "status": "success"}
+# ]
+
+# processed = []
+
+# for i in transactions:
+#     if i["status"] == "success":
+#         amount_pln = i["amount_usd"] * 4
+#         i["amount_pln"] = amount_pln
+
+#         processed.append(i)
+
+# print(processed)
+
+# matches = [
+#     {"player": "Daulet", "hero": "Sniper", "kills": 12},
+#     {"player": "Roman", "hero": "Pudge", "kills": 5},
+#     {"player": "Daulet", "hero": "Invoker", "kills": 8},
+#     {"player": "Karim", "hero": "Crystal Maiden", "kills": 2},
+#     {"player": "Roman", "hero": "Axe", "kills": 10}
+# ]
+
+# total_kills = {}
+
+# for user in matches:
+#     player_name = user["player"]
+#     match_kills = user["kills"]
+#     if player_name not in total_kills:
+#         total_kills[player_name] = match_kills
+#     else:
+#         total_kills[player_name] += match_kills
+
+api_response = {
+    "status": "200 OK",
+    "data": {
+        "users": [
+            {"info": {"name": "Daulet", "email": "daulet@example.com"}, "is_active": True},
+            {"info": {"name": "Roman", "email": "roman@example.com"}, "is_active": False},
+            {"info": {"name": "Karim", "email": "karim@example.com"}, "is_active": True}
+        ]
+    }
+}
+
+active_emails = []
+
+for user in api_response["data"]["users"]:
+    mail = user["info"]["email"]
+    if user["is_active"]:
+        active_emails.append(mail)
+
+print(active_emails)
