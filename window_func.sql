4caffa1c8ea35b28d8356a9beee1be648c403767
daulet <shabakovdaulet@gmail.com>
2026-06-15 23:13:51 +0200

    sql prac

--- window_func.sql
+++ window_func.sql
@@ -1,3 +1,7 @@
-SELECT user_id, sum(amount) as total_spent
-FROM user_purchases
-GROUP BY user_id
+-- SELECT user_id, sum(amount) as total_spent
+-- FROM user_purchases
+-- GROUP BY user_id
+
+SELECT users.name, transactions.txn_id, amount
+FROM users
+join transactions ON users.user_id = transactions.user_id
