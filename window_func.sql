85bf3b8c23e27980f73b5c9afa93289f16207883
daulet <shabakovdaulet@gmail.com>
2026-06-16 21:34:04 +0200

    sql pract

--- window_func.sql
+++ window_func.sql
@@ -2,6 +2,18 @@
 -- FROM user_purchases
 -- GROUP BY user_id
 
-SELECT users.name, transactions.txn_id, amount
-FROM users
-join transactions ON users.user_id = transactions.user_id
+-- SELECT users.name, transactions.txn_id, amount
+-- FROM users
+-- join transactions ON users.user_id = transactions.user_id
+
+-- SELECT users.name, sum(amount) as total_spent
+-- FROM users
+-- join transactions ON users.user_id = transactions.user_id
+-- GROUP BY users.user_id, users.name
+
+-- SELECT users.name, sum(amount) as total_spent
+-- FROM users
+-- join transactions ON users.user_id = transactions.user_id
+-- GROUP BY users.user_id, users.name
+-- having total_spent > 1000
+
